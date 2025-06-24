01c46f99ec9c034bc1d46697e175299b25c7550a
Kyle Francis <sjack421@gmail.com>
2025-06-24 12:26:23 -0400

    Worked on the sign in and the sign out functionality, plan on finishing the session set up

--- doc_agent/frontend/src/connections/user-connections.ts
+++ doc_agent/frontend/src/connections/user-connections.ts
@@ -6,17 +6,17 @@ import { validatePassword } from "../functions/passwordValidator"; // brought in
 // function to sign up the user
 export const signUpSupabase = async (email: string, password: string) => {
   try {
-    // check the users password for validity
+    // check the users password to make sure that it meets the requirements
     const checkedPassword = validatePassword(password);
 
-    // check if the password is valid
+    // if the password is not valid, show an error message and exit the function
     if (checkedPassword) {
       toast.error(checkedPassword);
       return; // return if the password is not valid
     }
 
     // continue otherwise
-    const { data, error } = await supabase.auth.signUp({
+    const { data, error: signUpError } = await supabase.auth.signUp({
       email: email,
       password: password,
       // options: {
@@ -28,11 +28,48 @@ export const signUpSupabase = async (email: string, password: string) => {
       toast.success("You have successfully signed up!");
     }
     // handle any errors during sign in
-    if (error) {
-      toast.error("Sign up failed. Please try again.");
+    if (signUpError) {
+      toast.error(`Sign up failed ${signUpError.message}. Please try again.`);
       return;
     }
-  } catch (error) {
-    console.error(error);
+  } catch (tryError) {
+    console.error(tryError);
   }
 };
+
+export const signInSupabase = async (email: string, password: string) => {
+  try {
+    // try to sign in the user
+    const { data, error: signInError } = await supabase.auth.signInWithPassword(
+      { email, password }
+    );
+
+    // show if there was an error during the sign in process
+    if (signInError) {
+      toast.error(`Sign in failed: ${signInError.message}`);
+      //exit the function if the sign in fails
+      return;
+    }
+
+    //if the sign in is successful, show a success message
+    if (data) {
+      toast.success(`You are signed in`);
+    }
+    // if there was a problem with the sign in process, show an error message
+  } catch (tryError) {
+    toast.error(`There was a problem signing in: ${tryError} `);
+  }
+};
+
+// sign out function for the user
+export const SignOutSupabase = async () => {
+  //try to sign out the user
+  const { error: signOutError } = await supabase.auth.signOut();
+
+  // if there was an error during the sign out process, show an error message
+  if (signOutError) {
+    toast.error(`Sign out failed: ${signOutError.message}`);
+  }
+};
+
+// function to check the user session and if they are signed in currently (might handle this in the main app component)
