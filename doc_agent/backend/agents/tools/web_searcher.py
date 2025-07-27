34937fdc734170998f72c589b9a7ea1668dc1f1a
Kyle Francis <sjack421@gmail.com>
2025-07-27 09:29:43 -0400

    Set up base search function
    still have to refactor later for better return response and fetching the
    needed data

--- doc_agent/backend/agents/tools/web_searcher.py
+++ doc_agent/backend/agents/tools/web_searcher.py
@@ -1,1 +1,32 @@
-# This file will have the web searcher tool that will be used to search the web for information
+import requests
+
+# TODO: refactor to search the web for specific queries that I need Doc to return the right answer to.
+
+
+# function for searching the web using DuckDuckGo
+def web_search(query: str):
+    """
+    This is a function for searching the web using DuckDuckGo.
+    This should only search for the top 5 results, and only if Doc doesn't know the answer.
+
+    query: str
+        The search query to be used for the web search.
+        (users question will be passed here)
+    """
+
+    # url to access the DuckDuckGo search API
+    url = "https://www.searchapi.io/api/v1/search"
+    # params for the search API
+    params = {
+        "engine": "duckduckgo",
+        "q": query,
+    }
+
+    # sending the request to the DuckDuckGo search API
+    response = requests.get(url, params=params)
+
+    if response.status_code == 400:
+        return "Error: Bad Request. Please check your query."
+
+    if response.status_code == 200:
+        return response.text
--- doc_agent/backend/schemas/chat_schema.py
+++ doc_agent/backend/schemas/chat_schema.py
@@ -1,46 +1,54 @@
-#Import the base model from pydantic so that I can create a model for these chat pieces
-from pydantic import BaseModel # create default model for the responses from the api
+# Import the base model from pydantic so that I can create a model for these chat pieces
+from pydantic import BaseModel  # create default model for the responses from the api
 
 # import langchain Ai message that the Agent gives back as a default response in this format
-from langchain_core.messages import AIMessage
+# from langchain_core.messages import AIMessage
 
-#typing model will help me when developing an making connections with older versions of python
-from typing import List # allows me to type the parameters for the objects I make 
 
-class MessageRequest(BaseModel): # use base model to define the schema for this chat request
+class MessageRequest(
+    BaseModel
+):  # use base model to define the schema for this chat request
     """Message request from the user with the custom session and the users ID"""
-    question : str 
-    session_id : str #string to that the session can be saved in the database
-    user_id : str # id of the user that sends the request
-    role: str # role will be filled automatically
 
-#returned to in json format to the user
-class DocsResponse(BaseModel): # structure docs response
+    question: str
+    session_id: str  # string to that the session can be saved in the database
+    user_id: str  # id of the user that sends the request
+    role: str  # role will be filled automatically
+
+
+# returned to in json format to the user
+class DocsResponse(BaseModel):  # structure docs response
     """Doc's Response to the user, giving the status of success, the user question and the answer to it"""
-    status : str # status if the message was successful or not
-    user_question : str # the users message
-    docs_response : str # Doc's response
+
+    status: str  # status if the message was successful or not
+    user_question: str  # the users message
+    docs_response: str  # Doc's response
+
 
 # might not be needed
 class SessionRequest(BaseModel):
     """Request format to get the information about messages that match the session id"""
-    session_id : str
+
+    session_id: str
     # might add the user id for validation soon
+
 
 # format for the message that I get back from the database
 class SessionMessage(BaseModel):
     """Message format that I get back from the database"""
-    session_id : str
-    role : str
-    content : str
-    user_id : str
+
+    session_id: str
+    role: str
+    content: str
+    user_id: str
     pass
+
 
 # might not be needed
 class SessionMessages(BaseModel):
     """Response format to return the messages and the information for the messages that match session_id"""
-    session_id : str
-    messages : str
-    user_id : str
-    pass
 
+    session_id: str
+    messages: str
+    user_id: str
+    pass
